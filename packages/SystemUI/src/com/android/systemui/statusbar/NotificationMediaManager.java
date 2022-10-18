211f78de1a09bb58be0d65e160d03d2b003a3927
Pranav Vashi <neobuddy89@gmail.com>
2022-10-19 00:41:10 +0530

    Pulse: Implement pulse color based album art [1/2]
    Change-Id: I8dd0543b2b071745d07401dccb67980c6b9c451b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -35,6 +35,7 @@ import android.util.Log;
 import androidx.annotation.VisibleForTesting;
 
 import com.android.systemui.Dumpable;
+import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dump.DumpManager;
@@ -94,6 +95,8 @@ public class NotificationMediaManager implements Dumpable {
     private String mMediaNotificationKey;
     private MediaMetadata mMediaMetadata;
 
+    private final SysuiColorExtractor mColorExtractor;
+
     @VisibleForTesting
     final MediaController.Callback mMediaListener = new MediaController.Callback() {
         @Override
@@ -141,7 +144,8 @@ public class NotificationMediaManager implements Dumpable {
             MediaDataManager mediaDataManager,
             DumpManager dumpManager,
             @Background Executor backgroundExecutor,
-            @Main Handler handler
+            @Main Handler handler,
+            SysuiColorExtractor colorExtractor
     ) {
         mContext = context;
         mMediaListeners = new ArrayList<>();
@@ -151,6 +155,7 @@ public class NotificationMediaManager implements Dumpable {
         mNotifCollection = notifCollection;
         mBackgroundExecutor = backgroundExecutor;
         mHandler = handler;
+        mColorExtractor = colorExtractor;
 
         setupNotifPipeline();
 
@@ -292,11 +297,15 @@ public class NotificationMediaManager implements Dumpable {
 
     private void updateMediaMetaData(MediaListener callback) {
         int playbackState = getMediaControllerPlaybackState(mMediaController);
-        mHandler.post(
-                () -> callback.onPrimaryMetadataOrStateChanged(mMediaMetadata, playbackState));
+        mHandler.post(() -> {
+            callback.onPrimaryMetadataOrStateChanged(mMediaMetadata, playbackState);
+            callback.setMediaNotificationColor(mColorExtractor.getMediaBackgroundColor());
+        });
     }
 
     public void removeCallback(MediaListener callback) {
+        mColorExtractor.setMediaBackgroundColor(0);
+        callback.setMediaNotificationColor(0);
         mMediaListeners.remove(callback);
     }
 
@@ -441,6 +450,7 @@ public class NotificationMediaManager implements Dumpable {
         mHandler.post(() -> {
             for (int i = 0; i < callbacks.size(); i++) {
                 callbacks.get(i).onPrimaryMetadataOrStateChanged(mMediaMetadata, state);
+                callbacks.get(i).setMediaNotificationColor(mColorExtractor.getMediaBackgroundColor());
             }
         });
     }
@@ -509,5 +519,7 @@ public class NotificationMediaManager implements Dumpable {
          */
         default void onPrimaryMetadataOrStateChanged(MediaMetadata metadata,
                 @PlaybackState.State int state) {}
+
+        default void setMediaNotificationColor(int color) {};
     }
 }
