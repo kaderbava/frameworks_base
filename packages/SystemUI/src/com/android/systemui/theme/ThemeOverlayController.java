LeafOS-Project/android_frameworks_base

SystemUI: Add brightness slider styles [1/3]
Change-Id: I0f2c05a3d0b83ba9ddaca23e08e97e87a5c69a2c

packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -500,6 +500,30 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                 },
                 UserHandle.USER_ALL);
 
+        mSecureSettings.registerContentObserverForUser(
+                Settings.Secure.getUriFor(Settings.Secure.BRIGHTNESS_SLIDER_STYLE),
+                false, new ContentObserver(mBgHandler) {
+                    @Override
+                    public void onChange(
+                            boolean selfChange, Collection<Uri> collection, int flags, int userId) {
+                        if (DEBUG)
+                            Log.d(TAG, "Overlay changed for user: " + userId);
+                        if (mUserTracker.getUserId() != userId) {
+                            return;
+                        }
+                        if (!mDeviceProvisionedController.isUserSetup(userId)) {
+                            Log.i(TAG, "Theme application deferred when setting changed.");
+                            mDeferredThemeEvaluation = true;
+                            return;
+                        }
+                        int brightnessSliderStyle =
+                                Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                                        Settings.Secure.BRIGHTNESS_SLIDER_STYLE, 0,
+                                        UserHandle.USER_CURRENT);
+                        mThemeManager.setBrightnessSliderStyle(brightnessSliderStyle);
+                    }
+                }, UserHandle.USER_ALL);
+
         if (!mIsMonetEnabled) {
             return;
         }

kaderbava/frameworks_base

packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -554,6 +554,30 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                 },
                 UserHandle.USER_ALL);
 
+        mSecureSettings.registerContentObserverForUserSync(
+                Settings.Secure.getUriFor(Settings.Secure.BRIGHTNESS_SLIDER_STYLE),
+                false, new ContentObserver(mBgHandler) {
+                    @Override
+                    public void onChange(
+                            boolean selfChange, Collection<Uri> collection, int flags, int userId) {
+                        if (DEBUG)
+                            Log.d(TAG, "Overlay changed for user: " + userId);
+                        if (mUserTracker.getUserId() != userId) {
+                            return;
+                        }
+                        if (!mDeviceProvisionedController.isUserSetup(userId)) {
+                            Log.i(TAG, "Theme application deferred when setting changed.");
+                            mDeferredThemeEvaluation = true;
+                            return;
+                        }
+                        int brightnessSliderStyle =
+                                Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                                        Settings.Secure.BRIGHTNESS_SLIDER_STYLE, 0,
+                                        UserHandle.USER_CURRENT);
+                        mThemeManager.setBrightnessSliderStyle(brightnessSliderStyle);
+                    }
+                }, UserHandle.USER_ALL);
+
         // All wallpaper color and keyguard logic only applies when Monet is enabled.
         if (!mIsMonetEnabled) {
             return;